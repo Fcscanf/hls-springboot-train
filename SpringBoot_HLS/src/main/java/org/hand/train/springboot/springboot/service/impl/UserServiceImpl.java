12b3c48fc142f8c4934b0e9e2e5e7d65fadd6088
Fcant <fcscanf@outlook.com>
2019-12-03 20:24:44 +0800

    [Code-Updates]:添加注解

--- SpringBoot_HLS/src/main/java/org/hand/train/springboot/springboot/service/impl/UserServiceImpl.java
+++ SpringBoot_HLS/src/main/java/org/hand/train/springboot/springboot/service/impl/UserServiceImpl.java
@@ -22,26 +22,65 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserMapper userMapper;
 
+    /**
+     * 查询所有用户
+     *
+     * @return List<UserInfo>
+     * @author Fcant
+     * @date 18:39 2019/12/3
+     */
     @Override
     public List<UserInfo> selectAllUser() {
         return userMapper.selectAllUser();
     }
 
+    /**
+     * 根据用户ID查询用户信息
+     *
+     * @param id
+     * @return UserInfo
+     * @author Fcant
+     * @date 18:40 2019/12/3
+     */
     @Override
     public UserInfo selectUserById(int id) {
         return userMapper.selectUserById(id);
     }
 
+    /**
+     * 添加用户
+     *
+     * @param userInfo
+     * @return int
+     * @author Fcant
+     * @date 18:40 2019/12/3
+     */
     @Override
     public int addUser(UserInfo userInfo) {
         return userMapper.addUser(userInfo);
     }
 
+    /**
+     * 更新用户信息
+     *
+     * @param userInfo
+     * @return int
+     * @author Fcant
+     * @date 18:41 2019/12/3
+     */
     @Override
     public int updateUser(UserInfo userInfo) {
         return userMapper.updateUser(userInfo);
     }
 
+    /**
+     * 删除用户
+     *
+     * @param userInfo
+     * @return int
+     * @author Fcant
+     * @date 18:41 2019/12/3
+     */
     @Override
     public int delUser(UserInfo userInfo) {
         return userMapper.delUser(userInfo);
